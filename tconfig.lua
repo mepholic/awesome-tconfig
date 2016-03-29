af9655d328d8e640194e19c58a460069b8c11d3b
mepholic <gikid0@gmail.com>
2016-03-29 05:29:51 -0400

    Initial commit of awesome toml configuration

---
+++ tconfig.lua
@@ -0,0 +1,22 @@
+-- mepholic's stuff
+local toml = require("toml")
+
+-- Read in YAML configuraion to a variable
+local config_file = os.getenv("HOME") .. "/.config/awesome/config.toml"
+local config_fd = io.open(config_file, "r")
+io.input(config_fd)
+local config_raw = io.read("*all")
+io.close(config_fd)
+
+-- Parse YAML config
+config = toml.parse(config_raw)
+
+-- Put together public interface
+local tconfig = {}
+
+local dprofile = config.default_profile
+
+tconfig.profile = config.profiles
+tconfig.default = config.profiles.dprofile
+
+return tconfig
---
+++ toml.lua
@@ -0,0 +1,1 @@
+lua-toml/toml.lua
